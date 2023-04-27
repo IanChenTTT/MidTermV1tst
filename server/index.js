ff1a22ecbb9930ea47fa8351f4abf5c0163e1c65
Chen <ianchenhappy438@gmail.com>
2023-04-27 21:07:15 +0800

    Change_ServerRoute

--- server/index.js
+++ server/index.js
@@ -9,26 +9,14 @@ const path = require("path");
 const io = new Server(httpServer);
 const PORT = 3000 || process.env.PORT;
 console.log(PORT);
-//   , {
-//     path: "/ChessRoom"
-// });
-// console.log(path.sep)
-// console.log(path.join(__dirname,'..','client'))
 console.log(__dirname);
-app.use(express.static(path.join(__dirname, "..", "client")));
+app.use("/Loby",express.static(path.join(__dirname, "..", "client")));
 app.use(express.static(path.join(__dirname, "..", "public/Util")));
-app.use("/", (req, res) => {
-  res.end("<h1>Testing sokcet io and express is working or not</h1>");
-});
-// app.use("ChessRoom",(req,res)=>{
-//   res.send(path.join())
-// })
+
 app.all("*", (req, res) => {
   res.end("<h1>404 NOT FOUND</h1>");
 });
-// app.all('*',(req, res) => {
-//   res.status(404).send("resource not found");
-// });
+
 let counter = 0;
 io.on("connection", async (socket) => {
   // Client join Room
